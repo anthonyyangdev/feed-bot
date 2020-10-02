fa2c9e79eaf2770eaef5a71a73804d61b67e3c58
Thomas Lu <tal82@cornell.edu>
2020-10-02 16:28:53 -0700

    Added DM replies

--- src/index.ts
+++ src/index.ts
@@ -12,8 +12,19 @@ client.on("ready", () => {
   console.log(`Logged in as ${client.user?.tag}!`);
 });
 
+// event checks if message has been sent and reacts accordingly
 client.on("message", async (msg) => {
+  if (msg.channel.type === "dm" && !msg.author.bot) {
+    if (msg.content === "!commands") {
+      msg.reply("Here is a list of available commands!");
+    } else {
+      msg.reply("Hello <@" + msg.author.id + ">, how can I help you?\nYou can list available commands by typing !commands");
+    }
+  }
+
   if (msg.content.startsWith("!ping")) {
+    // checks the guild name and checks if channel name is general
+    // need to make it check if the name is DMChannel name
     const ch = msg.guild?.channels.cache.find(channel => channel.name === "general");
     if (ch != null) {
       msg.reply("Pong! " + ch.createdTimestamp + ". Found the general channel in the cache");
