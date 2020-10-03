dbe9caf6b3b26ac1ad7cb7b4a4dea76b3e80fadb
Thomas Lu <tal82@cornell.edu>
2020-10-02 22:38:01 -0700

    Added preliminary periodic message check code.

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,17 @@
 import Discord from 'discord.js';
-import {MessageModel} from './MessageStorage';
+import { MessageModel } from './MessageStorage';
 
 import path from 'path';
 import env from 'dotenv';
 import mongoose from "mongoose";
-import {UserModel} from "./collections/UserModel";
+import { UserModel } from "./collections/UserModel";
 env.config({
   path: path.join(__dirname, '..', '.env')
 });
+
+function callEveryHour() {
+  setInterval(saveMessages, 1000 * 60);
+}
 
 async function start(): Promise<void> {
   const url = "mongodb://localhost:27017/";
@@ -29,10 +33,63 @@ start();
 
 const client = new Discord.Client();
 
+// const messages = await MessageModel.find({});
+//     const message_links = await Promise.all(messages.map(async (v) => msg.channel.messages.fetch(v.message_id)));
+//     message_links.forEach(link => {
+//       msg.reply(link.content);
+//     });
+
+function containsKeywords(content: string, keywords: string[]): boolean {
+  var result = false;
+  keywords.forEach(keyword => {
+    if (content.includes(keyword)) {
+      result = result || true;
+    }
+  })
+  return result;
+}
+
+async function saveMessages() {
+  // const channel_id = client.channels;
+  // get all users and apply across users
+  // have it send as a dm to the user
+  // messages mapped by channels, and guild
+  const author_id = client.user?.id;
+  if (author_id != undefined) {
+    const user = await UserModel.findOne({ author_id });
+    if (user != null) {
+      const user_channels = user.channels;
+      const user_keywords = user.keywords;
+      user_channels.forEach(async channel_id => {
+        console.log("Saving messages in channel: " + channel_id);
+        const dc_channel = await client.channels.fetch(channel_id);
+        // get the discord Channel object with channel_id
+        // need to check timing of message
+        const msg_collector = await new Discord.MessageCollector(dc_channel as Discord.TextChannel, msg => containsKeywords(msg.content, user_keywords));
+        await msg_collector.checkEnd();
+        msg_collector.collected.forEach(async msg => {
+          const doc = await MessageModel.create({
+            author: msg.author.id,
+            message_id: msg.id,
+            channel_id: msg.channel.id
+          });
+          await doc.save();
+          console.log("Saved a new message " + msg.content);
+        });
+        console.log("Done saving.");
+      });
+      console.log("Saved messages");
+    }
+  } else {
+    console.log("User not found");
+  }
+}
+
+callEveryHour();
+
 client.on("ready", () => {
   console.log(`Logged in as ${client.user?.tag}!`);
 });
-
 
 // event checks if message has been sent and reacts accordingly
 client.on("message", async (msg) => {
@@ -51,7 +108,7 @@ client.on("message", async (msg) => {
   if (msg.content.trim() === "!save-channel") {
     const channel_id = msg.channel.id;
     const author_id = msg.author.id;
-    const user = await UserModel.findOne({author_id});
+    const user = await UserModel.findOne({ author_id });
     if (user == null) {
       const doc = await UserModel.create({
         author_id,
@@ -75,7 +132,7 @@ client.on("message", async (msg) => {
 
   if (msg.content.trim() === "!my-channels") {
     const author_id = msg.author.id;
-    const user = await UserModel.findOne({author_id});
+    const user = await UserModel.findOne({ author_id });
     if (user == null) {
       await msg.reply("You have no saved channels");
     } else {
