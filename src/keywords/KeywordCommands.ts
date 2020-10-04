532ea323a77a938baf0205bfd1526c99a1883cf1
Anthony Yang <ay339@cornell.edu>
2020-10-04 02:15:18 -0400

    Add interface for keyboard

---
+++ src/keywords/KeywordCommands.ts
@@ -0,0 +1,78 @@
+import {CommandInterface} from "../CommandInterface";
+import {Message} from "discord.js";
+import {parseKeywords} from "./parseKeywords";
+import {UserModel} from "../collections/UserModel";
+
+/**
+ * Contains commands related to keywords.
+ */
+export const KeyboardCommands: {
+  add: CommandInterface;
+  remove: CommandInterface;
+  show: CommandInterface;
+} = {
+  show: {
+    command: "!show-keywords",
+    description: "",
+    async checkAndRun(msg: Message): Promise<void> {
+      const msg_input = msg.content.trim();
+      const author_id = msg.author.id;
+      if (msg_input.startsWith(this.command)) {
+        const user = await UserModel.findOne({author_id});
+        if (user == null) {
+          await msg.reply("You don't have any channels saved");
+        } else {
+          await msg.reply("Keywords: " + user.keywords.map(v => "<" + v + ">").join(", "));
+        }
+      }
+    }
+  },
+  remove: {
+    command: "!remove-keywords",
+    description: "Removes keywords from the user's profile. Multi-word phrases can be added by using" +
+      " double-quotation marks.",
+    async checkAndRun(msg: Message): Promise<void> {
+      const msg_input = msg.content.trim();
+      const author_id = msg.author.id;
+      if (msg_input.startsWith(this.command)) {
+        const keywords = parseKeywords(msg_input, this.command);
+        const user = await UserModel.findOne({author_id});
+        if (user == null) {
+          await msg.reply("You don't have any channels saved");
+        } else {
+          await UserModel.findOneAndUpdate({author_id}, {
+            $pullAll: {
+              keywords: keywords
+            }
+          });
+        }
+      }
+    }
+  },
+  add: {
+    command: "!add-keywords",
+    description: "Add keywords onto the user's profile for the bot to use when curating content. Multi-word phrases" +
+      " can be added by using double-quotation marks.",
+    async checkAndRun(msg: Message): Promise<void> {
+      const msg_input = msg.content.trim();
+      const author_id = msg.author.id;
+      if (msg_input.startsWith(this.command)) {
+        const keywords = parseKeywords(msg_input, this.command);
+        const user = await UserModel.findOne({author_id});
+        if (user == null) {
+          await msg.reply("You don't have any channels saved");
+        } else if (keywords != null) {
+          await UserModel.findOneAndUpdate({author_id}, {
+            $push: {
+              keywords: {
+                $each: keywords.filter(w => !user.keywords.includes(w)),
+                $slice: 100
+              }
+            }
+          });
+        }
+      }
+    }
+  }
+};
+
